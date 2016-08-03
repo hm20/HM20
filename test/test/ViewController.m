4d020e06fcfde7e34cc23465285ca564ab771b13
hm20 <HM@HMs-iMac.local>
2016-08-03 15:42:56 +0800

    打印

---
+++ test/test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by HM on 16/8/3.
+//  Copyright © 2016年 HM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"ddddd");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
